93272476417c1a6f2b706230a9a32944e04073ba
laurakhot <laurak_11@uw.edu      git config --global user.email laurak_11@uw.edu>
2025-04-13 22:54:02 -0700

    working on money struct

--- Sources/DomainModel/DomainModel.swift
+++ Sources/DomainModel/DomainModel.swift
@@ -9,7 +9,52 @@ struct DomainModel {
 // Money
 //
 public struct Money {
+    var amount : Int
+    var currency: String
+    
+    public init(amount: Int, currency: String) {
+        self.amount = amount
+        self.currency = currency
+    }
+    
+    func convert(_ currName : String) -> Money {
+        // normalizing to USD
+        var usdVal = 0
+        switch self.currency {
+        case "GBP":
+            usdVal = self.amount * 2
+        case "EUR":
+            usdVal = self.amount * 2 / 3
+        case "CAN":
+            usdVal = self.amount * 4 / 5
+        default:
+            usdVal = self.amount
+        }
+        
+        switch currName {
+        case "GBP":
+            usdVal /= 2
+        case "EUR":
+            usdVal = Int(Double(usdVal) * 1.5)
+        case "CAN":
+            usdVal = Int(Double(usdVal) * 1.25)
+        default:
+            break
+        }
+        return Money(amount: usdVal, currency: currName)
+        
+    }
+    
+//    func add(_ money : Money) -> Money {
+//        
+//    }
+//    
+//    func subtract(_ money : Money) -> Money {
+//        
+//    }
 }
+
+
 
 ////////////////////////////////////
 // Job
